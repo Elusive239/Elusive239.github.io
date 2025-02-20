c099200be60e9ef4e6c2943a5b6520f3f9b49a13
Elusive239 <56003608+Elusive239@users.noreply.github.com>
2025-02-20 02:52:10 -0500

    decided to start writing an html component that lets me write stuff in markdown, because i prefer it over HTML. Writing the parsing in C3/WASM, for funsies! we'll see how far this goes!

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let wasm = null;
-
 window.onload = async () => {
     wasm = await startWasm("./out.wasm");
     wasm.main();
+    window.customElements.define('noir-down', MarkDown);
 };
 
 async function startWasm(url) {
     const importObject = {
         env: { 
-            console_log: (buffer, buffer_len) => console.log(new TextDecoder().decode(new Uint8ClampedArray(wasm.memory.buffer, buffer, buffer_len))),
+            console_log: (buffer, bufferLen) => console.log(new TextDecoder().decode(new Uint8ClampedArray(wasm.memory.buffer, buffer, bufferLen))),
             // rand: () => Math.random(),
         },
     };      
@@ -23,4 +23,31 @@ async function startWasm(url) {
     );
     wasm._initialize();
     return wasm;
+}
+
+
+class MarkDown extends HTMLElement{
+    constructor(){
+        super();
+        const shadowRoot = this.attachShadow({mode: 'closed'});
+        let div = document.createElement('div');
+        shadowRoot.append(div);
+
+        let buffer = this.textContent;
+        buffer = buffer.trim();
+        const bufferLen = buffer.length;
+        const wasmStr = wasm.malloc(bufferLen * 8); //buffer size of textContent, to pass textContent to WASM.
+        for(let i = 0; i < bufferLen; i++){
+            wasm.set(wasmStr, i, buffer[i].charCodeAt(0));
+        }
+        const wSize = wasm.malloc(32); //1 int ptr
+        const outPtr = wasm.md_to_html(wasmStr, bufferLen, wSize);
+        const outSize = new Uint8ClampedArray(wasm.memory.buffer, wSize, 1);
+        const outText = new TextDecoder().decode(new Uint8ClampedArray(wasm.memory.buffer, outPtr, outSize));
+        // console.log(outText);
+        div.innerHTML = outText;
+        wasm.free(wasmStr);
+        wasm.free(wSize);
+        wasm.free(outPtr);
+    }
 }
